8b54205e363d447e7d70832919ceddc2112e114e
Guillaume Husta <g.husta@free.fr>
2023-07-21 21:12:46 +0200

    Update to Spring FWK 5.3.29 / Spring Security 5.7.10

--- src/main/java/fr/husta/test/config/WebSecurityConfiguration.java
+++ src/main/java/fr/husta/test/config/WebSecurityConfiguration.java
@@ -5,67 +5,61 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import static fr.husta.test.security.WebSecurityConstants.Roles.*;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration {
 
     @Bean
-    @Override
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // Note that the matchers are considered in order.
+                // Doc : https://docs.spring.io/spring-security/site/docs/current/reference/html5/index.html#jc-authorize-requests
+                .authorizeRequests(auth -> auth
+                        .antMatchers("/admin/superadmin/**").hasRole(SUPERADMIN)
+                        .antMatchers("/admin/**").hasRole(ADMIN)
+                        .antMatchers("/secured/**").hasAnyRole(USER, SUPERUSER)
+                        // '/mixed/**' : To be refined in actions with annotations
+                        .antMatchers("/mixed/**").hasAnyRole(USER, SUPERUSER)
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults())
+                .anonymous(ano -> ano.disable())
+                .servletApi(withDefaults())
+                .csrf(withDefaults())
+                .headers(withDefaults());
+        return http.build();
+    }
+
+    @Bean
     public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         UserDetails userDetails = User.withDefaultPasswordEncoder()
                 .username("user").password("password").roles(USER)
                 .build();
-        manager.createUser(userDetails);
 
         UserDetails superuserDetails = User.withDefaultPasswordEncoder()
                 .username("superuser").password("password").roles(SUPERUSER)
                 .build();
-        manager.createUser(superuserDetails);
 
         UserDetails userAdmin = User.withDefaultPasswordEncoder()
                 .username("admin").password("admin*").roles(ADMIN)
                 .build();
-        manager.createUser(userAdmin);
 
         UserDetails god = User.withDefaultPasswordEncoder()
                 .username("god").password("jesus").roles(USER, ADMIN, SUPERADMIN)
                 .build();
-        manager.createUser(god);
 
-        return manager;
+        return new InMemoryUserDetailsManager(userDetails, superuserDetails, userAdmin, god);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                // Note that the matchers are considered in order.
-                // Doc : https://docs.spring.io/spring-security/site/docs/current/reference/html5/index.html#jc-authorize-requests
-                .antMatchers("/admin/superadmin/**").hasRole(SUPERADMIN)
-                .antMatchers("/admin/**").hasRole(ADMIN)
-                .antMatchers("/secured/**").hasAnyRole(USER, SUPERUSER)
-                // '/mixed/**' : To be refined in actions with annotations
-                .antMatchers("/mixed/**").hasAnyRole(USER, SUPERUSER)
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().and()
-                .httpBasic()
-                .and()
-                .anonymous().disable()
-                .servletApi()
-                .and()
-                .csrf()
-                .and()
-                .headers();
-    }
 }
